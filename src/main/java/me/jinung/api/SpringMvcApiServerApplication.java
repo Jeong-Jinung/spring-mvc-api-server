99b18ad163b19a2a372fb39307fd0433b25534e6
JeongJinUng <jinung.dev@gmail.com>
2023-03-16 14:21:49 +0900

    initial commit

---
+++ src/main/java/me/jinung/api/SpringMvcApiServerApplication.java
@@ -0,0 +1,13 @@
+package me.jinung.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcApiServerApplication.class, args);
+    }
+
+}
